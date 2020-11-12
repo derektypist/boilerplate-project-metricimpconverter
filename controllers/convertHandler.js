ab7e78213473ef928fa6be467e25672c724c4970
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2020-11-12 17:09:12 +0000

    Update convertHandler.js

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -6,29 +6,99 @@
 *       
 */
 
+let inputRegex = /[a-z]+|[^a-z]+/gi;
+
 function ConvertHandler() {
   
   this.getNum = function(input) {
     var result;
-    
+    result = input.match(inputRegex)[0];
+    let numberRegex = /\d/;
+    if (numberRegex.test(result) === false) {
+      result = 1;
+    }
+
+    if (result.toString().includes('/')) {
+      let values = result.toString().split('/');
+      if (values.length !=2) {
+        return 'invalid number';
+      }
+      values[0] = parseFloat(values[0]);
+      values[1] = parseFloat(values[1]);
+      result = parseFloat(values[0]/values[1].toFixed(5));
+    }
+
+    if (isNaN(result)) {
+      return 'invalid number';
+    }
     return result;
   };
   
   this.getUnit = function(input) {
     var result;
-    
+    result = input.match(inputRegex)[1];
+    if (!result) {
+      result = input.match(inputRegex)[0];
+    }
+
+    let validUnits = ['gal', 'l', 'mi', 'km', 'lbs', 'kg', 'GAL', 'L', 'MI', 'KM', 'LBS', 'KG'];
+
+    if (!validUnits.includes(result)) {
+      return 'invalid unit';
+    }
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
     var result;
-    
+    if (initUnit === 'gal' || initUnit === 'GAL') {
+      result = 'l';
+    } else if (initUnit === 'l' || initUnit === 'L') {
+      result = 'gal';
+    }
+
+    if (initUnit === 'lbs' || initUnit === 'LBS') {
+      result = 'kg';
+    } else if (initUnit === 'kg' || initUnit === 'KG') {
+      result = 'lbs';
+    }
+
+    if (initUnit === 'mi' || initUnit === 'MI') {
+      result = 'km';
+    } else if (initUnit === 'km' || initUnit === 'KM') {
+      result = 'mi';
+    }
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     var result;
-    
+    switch (unit) {
+      case 'gal':
+      case 'GAL':
+        result = 'gallon(s)';
+        break;
+      case 'l':
+      case 'L':
+        result = 'litre(s)';
+        break;
+      case 'lbs':
+      case 'LBS':
+        result = 'pound(s)';
+        break;
+      case 'mi':
+      case 'MI':
+        result = 'mile(s)';
+        break;
+      case 'km':
+      case 'KM':
+        result = 'kilometre(s)';
+        break;
+      case 'kg':
+      case 'KG':
+        result = 'kilogram(s)';
+        break;
+    }
     return result;
   };
   
