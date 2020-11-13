0ce9acca6daca971ba92ac96780727c21dac6dec
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2020-11-13 10:14:19 +0000

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -34,23 +34,48 @@ suite('Functional Tests', function() {
       });
       
       test('Convert 32g (invalid input unit)', function(done) {
+         chai.request(server)
+        .get('/api/convert')
+        .query({input: '32g'})
+        .end(function(err, res) {
+          assert.equal(res.body, 'invalid unit');
+        });
+        done();
         
-        //done();
       });
       
       test('Convert 3/7.2/4kg (invalid number)', function(done) {
+        chai.request(server)
+        .get('/api/convert')
+        .query({input: '3/7.2/4kg'})
+        .end(function(err,res) {
+          assert.equal(res.body, 'invalid number');
+        });
+        done();
         
-        //done();
       });  
       
       test('Convert 3/7.2/4kilomegagram (invalid number and unit)', function(done) {
-        
-        //done();
+        chai.request(server)
+        .get('/api/convert')
+        .query({input: '3/7.2/4kilomegagram'})
+        .end(function(err,res) {
+          assert.equal(res.body, 'invalid number and unit');
+        });
+        done();
+       
       });
       
       test('Convert kg (no number)', function(done) {
+        chai.request(server)
+        .get('/api/convert')
+        .query({input: 'kg'})
+        .end(function(err,res) {
+          assert.equal(res.body.initNum, 1);
+          assert.eqaul(res.body.initUnit, 'kg');
+        });
+        done();
         
-        //done();
       });
       
     });
